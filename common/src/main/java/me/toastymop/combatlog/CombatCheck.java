ba0bc22a06c0af423558af9b95db813b705e4ebb
Szczurek <kamilsecret8@gmail.com>
2023-02-01 13:03:39 +0100

    Implement preventing from using elytra during fights

--- common/src/main/java/me/toastymop/combatlog/CombatCheck.java
+++ common/src/main/java/me/toastymop/combatlog/CombatCheck.java
@@ -3,21 +3,27 @@ package me.toastymop.combatlog;
 import me.toastymop.combatlog.util.IEntityDataSaver;
 import me.toastymop.combatlog.util.TagData;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.network.ServerPlayerEntity;
-
-import java.util.Objects;
 
 public class CombatCheck {
     public static void CheckCombat(Entity entity) {
-        LivingEntity target = (LivingEntity) entity;
-        if (target instanceof PlayerEntity) {
-            if ((target.getAttacker() instanceof PlayerEntity) && ((ServerPlayerEntity) target).interactionManager.getGameMode().isSurvivalLike() && ((ServerPlayerEntity) Objects.requireNonNull(target.getAttacker())).interactionManager.getGameMode().isSurvivalLike()) {
-                TagData.setTagTime((IEntityDataSaver) target);
-                TagData.setTagTime((IEntityDataSaver) target.getAttacker());
+        // Contributor note: I have refactored this method a bit
+        // to make it a bit easier to read
+        // and remove some unnecessary code like multiple casts, instead of one
+        if (entity instanceof ServerPlayerEntity player) {
+            if ((player.getAttacker() instanceof ServerPlayerEntity attacker) && player.interactionManager.getGameMode().isSurvivalLike() && attacker.interactionManager.getGameMode().isSurvivalLike()) {
+                TagData.setTagTime((IEntityDataSaver) player);
+                if (CombatConfig.disableElytra) {
+                    // Stop the player who took damage from flying
+                    // The attacker will be able to fly until taking damage from another player or until it lands
+                    player.stopFallFlying();
+                    if (CombatConfig.attackerNoFly) {
+                        attacker.stopFallFlying();
+                    }
+                }
+                TagData.setTagTime((IEntityDataSaver) attacker);
             } else if (CombatConfig.allDamage) {
-                TagData.setTagTime((IEntityDataSaver) target);
+                TagData.setTagTime((IEntityDataSaver) player);
             }
         }
 
--- common/src/main/java/me/toastymop/combatlog/CombatConfig.java
+++ common/src/main/java/me/toastymop/combatlog/CombatConfig.java
@@ -8,12 +8,16 @@ public class CombatConfig {
     static File file = new File("./config/combatlog-common.toml");
     public static int combatTime;
     public static Boolean allDamage;
+    public static Boolean disableElytra;
+    public static Boolean attackerNoFly;
     public static String deathMessage;
     public static void getConfig() {
         if (!file.exists()) {
             try {
                 combatTime = 30;
                 allDamage = false;
+                disableElytra = false;
+                attackerNoFly = false;
                 deathMessage = "\" has died of cowardice\"";
 
                 file.getParentFile().mkdirs();
@@ -24,6 +28,12 @@ public class CombatConfig {
                 fos.println("\tcombatTime = "+ combatTime);
                 fos.println("\t#Weather a player should be put in combat from just other players or all damage(true is all damage false is just players)");
                 fos.println("\tallDamage = "+ allDamage.toString());
+                fos.println("\t#Weather when in comba elytra usage should be disabled.");
+                fos.println("\tdisableElytra = "+ disableElytra.toString());
+                fos.println("\t#Should the attacker be instantly stopped from flying?");
+                fos.println("\t#If true when attacking someone when flying on elytra you will fall mid air.");
+                fos.println("\t#If false you will be able to fly until you land or until you will get damaged.");
+                fos.println("\tattackerNoFly = "+ attackerNoFly.toString());
                 fos.println("\t#The message that shows up when I player disconnects while in combat");
                 fos.println("\t#If you don't add a space to the beginning it will look like \"(player)deathmessage\" instead of \"(player) deathmessage\" ingame");
                 fos.println("\tdeathMessage = "+ deathMessage);
@@ -38,6 +48,8 @@ public class CombatConfig {
             if((config.get("config.combatTime") instanceof Integer) && (config.get("config.allDamage") instanceof Boolean)  && (config.get("config.deathMessage") instanceof String)){
                 combatTime = config.getInt("config.combatTime");
                 allDamage = config.get("config.allDamage");
+                disableElytra = config.get("config.disableElytra");
+                attackerNoFly = config.get("config.attackerNoFly");
                 deathMessage = config.get("config.deathMessage").toString();
             } else {
                 File rename = new File("./config/combatlog-common.toml.bak");

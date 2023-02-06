52032a4f8bfe6d726d35fba6201a93df8c23ef72
SzczurekYT <67533827+SzczurekYT@users.noreply.github.com>
2023-02-06 02:24:26 +0100

    Make the mod as server side only

--- forge/src/main/java/me/toastymop/combatlog/forge/CombatLogForge.java
+++ forge/src/main/java/me/toastymop/combatlog/forge/CombatLogForge.java
@@ -2,15 +2,11 @@ package me.toastymop.combatlog.forge;
 
 import me.toastymop.combatlog.CombatConfig;
 import me.toastymop.combatlog.CombatLog;
-import net.minecraftforge.fml.IExtensionPoint;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.network.NetworkConstants;
 
 @Mod(CombatLog.MOD_ID)
 public class CombatLogForge {
     public CombatLogForge() {
         CombatConfig.getConfig();
-        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (a, b) -> true));
     }
 }

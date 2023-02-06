34c3f08838f2c5190638a48ef4f3d1eae1b5f617
WildToastyMop <WildToastyMop@gmail.com>
2023-02-05 21:17:48 -0600

    Server side only

--- forge/src/main/java/me/toastymop/combatlog/forge/CombatLogForge.java
+++ forge/src/main/java/me/toastymop/combatlog/forge/CombatLogForge.java
@@ -2,16 +2,11 @@ package me.toastymop.combatlog.forge;
 
 import me.toastymop.combatlog.CombatConfig;
 import me.toastymop.combatlog.CombatLog;
-import net.minecraftforge.fml.ExtensionPoint;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.network.FMLNetworkConstants;
-import org.apache.commons.lang3.tuple.Pair;
 
 @Mod(CombatLog.MOD_ID)
 public class CombatLogForge {
     public CombatLogForge() {
         CombatConfig.getConfig();
-        ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
     }
 }

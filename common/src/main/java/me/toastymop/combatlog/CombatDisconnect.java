8e48070fe4c27e15c38c1fb0dc7314d705404860
WildToastyMop <WildToastyMop@gmail.com>
2023-11-07 10:57:10 -0600

    1.19.4
    I stopped procrastinating :D

--- common/src/main/java/me/toastymop/combatlog/CombatDisconnect.java
+++ common/src/main/java/me/toastymop/combatlog/CombatDisconnect.java
@@ -2,16 +2,31 @@ package me.toastymop.combatlog;
 
 import me.toastymop.combatlog.util.IEntityDataSaver;
 import me.toastymop.combatlog.util.TagData;
+import net.minecraft.network.message.MessageType;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Text;
+import net.minecraft.world.GameRules;
+
+import java.util.Objects;
+
+import static net.minecraft.world.GameRules.SHOW_DEATH_MESSAGES;
 
 
 public class CombatDisconnect {
 
     public static void OnPlayerDisconnect(ServerPlayerEntity entity) {
-        CombatDeathMessage damageSource = new CombatDeathMessage(CombatDeathMessage.COMBATLOG, entity);
-        damageSource.setBypassesArmor();
         if (TagData.getCombat((IEntityDataSaver) entity)) {
-            entity.damage(damageSource,1000);
+            // if anyone knows how to make this less terrible please help me im begging I tried using the new damagetypes for soooo long
+            GameRules.BooleanRule gamerule = Objects.requireNonNull(entity.getServer()).getGameRules().get(SHOW_DEATH_MESSAGES);
+            Text deathMessage = Text.of(entity.getDisplayName().getString()+CombatConfig.deathMessage);
+            if (gamerule.get()){
+                gamerule.set(false, entity.getServer());
+                entity.damage(entity.getDamageSources().outOfWorld(), 100000);
+                gamerule.set(true, entity.getServer());
+                entity.getServer().getPlayerManager().broadcast(deathMessage,false);
+            } else {
+                entity.damage(entity.getDamageSources().outOfWorld(), 100000);
+            }
             TagData.endCombat((IEntityDataSaver) entity);
         }
     }

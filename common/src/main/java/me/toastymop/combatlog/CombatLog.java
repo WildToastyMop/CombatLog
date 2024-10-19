db876d5f1c1be07060bf610107b2e334cc1bcec3
WildToastyMop <WildToastyMop@gmail.com>
2024-10-19 16:34:53 -0500

    started fixing, still broke

--- common/src/main/java/me/toastymop/combatlog/CombatLog.java
+++ common/src/main/java/me/toastymop/combatlog/CombatLog.java
@@ -1,6 +1,9 @@
 package me.toastymop.combatlog;
 
+import org.apache.logging.log4j.Logger;
+
 public class CombatLog {
     public static final String MOD_ID = "combatlog";
+    public static final String LOGGER = "CombatLog";
 
 }

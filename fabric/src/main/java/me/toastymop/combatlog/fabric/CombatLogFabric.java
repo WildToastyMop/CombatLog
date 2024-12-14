e2793434c2e90de3170544f97bdbf55125af9e11
WildToastyMop <WildToastyMop@gmail.com>
2024-12-14 16:55:22 -0600

    game, dependencies, and config update

--- fabric/src/main/java/me/toastymop/combatlog/fabric/CombatLogFabric.java
+++ fabric/src/main/java/me/toastymop/combatlog/fabric/CombatLogFabric.java
@@ -1,13 +1,11 @@
 package me.toastymop.combatlog.fabric;
 
 import me.toastymop.combatlog.CombatConfig;
-import net.fabricmc.api.DedicatedServerModInitializer;
 import net.fabricmc.api.ModInitializer;
 
 public class CombatLogFabric implements ModInitializer {
-    //dont know why this got broke if anyone know why please help
     @Override
     public void onInitialize() {
-        CombatConfig.getConfig();
+        CombatConfig.CONFIG = CombatConfig.load();
     }
 }

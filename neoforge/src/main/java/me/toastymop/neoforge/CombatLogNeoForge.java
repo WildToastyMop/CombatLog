ba8d420018bc4120cfce4849ff2ea1622703f2ec
WildToastyMop <WildToastyMop@gmail.com>
2025-03-09 18:29:37 -0500

    added neoforge support and new config options

---
+++ neoforge/src/main/java/me/toastymop/neoforge/CombatLogNeoForge.java
@@ -0,0 +1,13 @@
+package me.toastymop.neoforge;
+
+import me.toastymop.combatlog.CombatConfig;
+import me.toastymop.combatlog.CombatLog;
+import net.neoforged.fml.common.Mod;
+
+
+@Mod(CombatLog.MOD_ID)
+public class CombatLogNeoForge {
+    public CombatLogNeoForge() {
+        CombatConfig.CONFIG = CombatConfig.load();
+    }
+}

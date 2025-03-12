670cd9b1f43a2861a0ff1f41aecb2609565cca7f
WildToastyMop <WildToastyMop@gmail.com>
2025-03-12 18:15:48 -0500

    actually fixed fabric refmap

--- common/src/main/java/me/toastymop/combatlog/mixin/TickMixin.java
+++ common/src/main/java/me/toastymop/combatlog/mixin/TickMixin.java
@@ -6,10 +6,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-
 import static me.toastymop.combatlog.CombatTicks.CombatTick;
-
-
 
 @Mixin(MinecraftServer.class)
 public abstract class TickMixin {

a7d2b0da9965275b7cae0213c7f5b07a16cc92c0
WildToastyMop <WildToastyMop@gmail.com>
2025-07-25 15:41:02 -0500

    1.21.6 update

--- forge/src/main/java/me/toastymop/combatlog/forge/CombatEventHandler.java
+++ forge/src/main/java/me/toastymop/combatlog/forge/CombatEventHandler.java
@@ -2,14 +2,13 @@ package me.toastymop.combatlog.forge;
 
 import me.toastymop.combatlog.CombatTicks;
 import net.minecraftforge.event.TickEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.eventbus.api.listener.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber(modid = "combatlog", bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class CombatEventHandler {
     @SubscribeEvent
-    public static void onTick(TickEvent.ServerTickEvent event){
-        if (event.phase != TickEvent.Phase.END) return;
+    public static void onTick(TickEvent.ServerTickEvent.Post event){
         CombatTicks.CombatTick(event.getServer());
     }
 }

903923fac1fc8180c93fd3f091803dd922566f16
codestech <56263050+Codestech1@users.noreply.github.com>
2025-10-14 12:44:46 +0200

    Minecraft 1.21.10 support

--- common/src/main/java/me/toastymop/combatlog/CombatDisconnect.java
+++ common/src/main/java/me/toastymop/combatlog/CombatDisconnect.java
@@ -5,13 +5,13 @@ import com.mojang.brigadier.ParseResults;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import me.toastymop.combatlog.util.IEntityDataSaver;
 import me.toastymop.combatlog.util.TagData;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.text.Text;
 import net.minecraft.world.GameRules;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 import java.util.Objects;
 
@@ -21,24 +21,28 @@ import static net.minecraft.world.GameRules.SHOW_DEATH_MESSAGES;
 public class CombatDisconnect {
     public static void OnPlayerDisconnect(ServerPlayerEntity entity) {
         if (TagData.getCombat((IEntityDataSaver) entity)) {
+            ServerWorld world = entity.getEntityWorld();
+            MinecraftServer server = world.getServer();
+
             if (CombatConfig.Config.disconnectKill) {
                 // if anyone knows how to make this less terrible please help me im begging I tried using the new damagetypes for soooo long
-                GameRules.BooleanRule gamerule = Objects.requireNonNull(entity.getServer()).getGameRules().get(SHOW_DEATH_MESSAGES);
+                GameRules.BooleanRule gamerule = Objects.requireNonNull(server).getGameRules().get(SHOW_DEATH_MESSAGES);
                 Text deathMessage = Text.of(entity.getDisplayName().getString() + CombatConfig.Config.deathMessage);
                 if (gamerule.get()) {
-                    gamerule.set(false, entity.getServer());
-                    entity.damage(entity.getWorld(),entity.getDamageSources().outOfWorld(), 100000);
-                    gamerule.set(true, entity.getServer());
-                    entity.getServer().getPlayerManager().broadcast(deathMessage, false);
+                    gamerule.set(false, server);
+                    entity.damage(world, entity.getDamageSources().outOfWorld(), 100000);
+                    gamerule.set(true, server);
+                    server.getPlayerManager().broadcast(deathMessage, false);
                 } else {
-                    entity.damage(entity.getWorld(),entity.getDamageSources().outOfWorld(), 100000);
+                    entity.damage(world, entity.getDamageSources().outOfWorld(), 100000);
                 }
             }
+
             String disconnectCommand = CombatConfig.Config.disconnectCommand.replace("{player}",entity.getName().getString());
             if (disconnectCommand != null && !disconnectCommand.trim().isEmpty()){
-                CommandManager manager = entity.getServer().getCommandManager();
+                CommandManager manager = server.getCommandManager();
                 CommandDispatcher<ServerCommandSource> dispatcher = manager.getDispatcher();
-                ServerCommandSource commandSource = entity.getServer().getCommandSource().withLevel(4);
+                ServerCommandSource commandSource = server.getCommandSource().withLevel(4);
                 ParseResults<ServerCommandSource> parseResults = dispatcher.parse(disconnectCommand,commandSource);
                 try {
                     dispatcher.execute(parseResults);
@@ -46,6 +50,7 @@ public class CombatDisconnect {
                     commandSource.sendError(Text.of(e.getMessage()));
                 }
             }
+
             TagData.endCombat((IEntityDataSaver) entity);
         }
     }

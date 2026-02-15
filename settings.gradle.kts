79dc9b68cadea8e0253d76bc1e6a7a116b14c612
Stimpy <95110502+StimpyDev@users.noreply.github.com>
2026-02-15 15:38:00 +0100

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -22,7 +22,7 @@ stonecutter {
         fun mc(loader: String, vararg versions: String) {
             for (version in versions) vers("$version-$loader", version)
         }
-        mc("fabric", "1.21.11", "1.21.6", "1.21.1", "1.20.1", "1.19.2", "1.16.5")
+        mc("fabric", "1.21.11", "1.21.10", "1.21.6", "1.21.1", "1.20.1", "1.19.2", "1.16.5")
         mc("forge", "1.20.1", "1.19.2", "1.16.5")
         mc("neoforge", "1.21.11", "1.21.6", "1.21.1")
     }

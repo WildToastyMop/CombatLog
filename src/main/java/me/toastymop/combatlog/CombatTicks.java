a2bec6c5632d0b649b8955c19a7098bba65b2ae8
WildToastyMop <46883460+WildToastyMop@users.noreply.github.com>
2026-07-25 11:11:39 -0500

    feat: end combat when all attackers leave or die

--- src/main/java/me/toastymop/combatlog/CombatTicks.java
+++ src/main/java/me/toastymop/combatlog/CombatTicks.java
@@ -2,6 +2,9 @@ package me.toastymop.combatlog;
 
 import me.toastymop.combatlog.util.IEntityDataSaver;
 import me.toastymop.combatlog.util.TagData;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.ListTag;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.network.chat.Style;
@@ -23,6 +26,51 @@ public class CombatTicks {
 
             int tagTime = TagData.getTagTime(data);
             boolean combatNotice = CombatConfig.Config.combatNotice;
+
+			if (tagTime % 20 == 0) {
+				CompoundTag persistentData = (data).getPersistentData();
+				//? if >1.21.5 {
+				ListTag attackers = persistentData.getListOrEmpty("attackerHistory");
+				//?} else {
+				/*ListTag attackers = persistentData.getList("attackerHistory", 10);
+				*///?}
+				boolean anyStillFighting = false;
+				for (int i = 0; i < attackers.size(); i++) {
+					//? if >1.21.5 {
+					CompoundTag entry = attackers.getCompoundOrEmpty(i);
+					//?} else {
+					/*CompoundTag entry = attackers.getCompound(i);
+					*///?}
+					//? if >1.21.5 {
+					String name = entry.getString("Name").orElse("");
+					//?} else {
+					/*String name = entry.getString("Name");
+					*///?}
+					ServerPlayer attacker = server.getPlayerList().getPlayerByName(name);
+					if (attacker != null && TagData.getCombat((IEntityDataSaver) attacker)) {
+						anyStillFighting = true;
+						break;
+					}
+				}
+				if (!anyStillFighting) {
+					TagData.endCombat(data);
+					if (combatNotice) {
+						//? if >=1.19 {
+						MutableComponent outCombat = Component.literal(CombatConfig.Config.outCombat);
+						//?} else {
+						/*TextComponent outCombat = new TextComponent(CombatConfig.Config.outCombat);
+						*///?}
+						//? if <=1.21.11 {
+						/*player.displayClientMessage(outCombat
+								.withStyle(Style.EMPTY.withColor(ChatFormatting.GREEN)), true);
+						*///?} else {
+						player.sendOverlayMessage(outCombat
+								.withStyle(Style.EMPTY.withColor(ChatFormatting.GREEN)));
+						//?}
+					}
+					continue;
+				}
+			}
 
             if (tagTime > 0) {
                 TagData.decreaseTagTime(data);

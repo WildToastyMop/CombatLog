ca21c34ce8c5907291fd3718e68fda0bd13deacd
WildToastyMop <WildToastyMop@gmail.com>
2026-07-26 10:17:37 -0500

    fix: refmap and mixin

--- src/main/java/me/toastymop/combatlog/mixin/EnderpearlMixin.java
+++ src/main/java/me/toastymop/combatlog/mixin/EnderpearlMixin.java
@@ -4,28 +4,23 @@ import me.toastymop.combatlog.util.IEntityDataSaver;
 import me.toastymop.combatlog.util.TagData;
 import net.minecraft.world.entity.Entity;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 //? if >=1.21.11 {
-import org.jspecify.annotations.Nullable;
 import net.minecraft.world.entity.projectile.throwableitemprojectile.ThrownEnderpearl;
 
 @Mixin(ThrownEnderpearl.class)
 //?} else {
-/*import org.jetbrains.annotations.Nullable;
-
-@Mixin(net.minecraft.world.entity.projectile.ThrownEnderpearl.class)
- *///?}
+/*@Mixin(net.minecraft.world.entity.projectile.ThrownEnderpearl.class)
+*///?}
 public abstract class EnderpearlMixin {
-	@Shadow
-	public abstract @Nullable Entity getOwner();
 
 	@Inject(method = "onHit", at = @At("HEAD"), cancellable = true)
 	private void injectPearlConfigMethod(CallbackInfo ci) {
-		if (TagData.getCombat((IEntityDataSaver) this.getOwner())){
+		Entity owner = ((net.minecraft.world.entity.projectile.Projectile)(Object)this).getOwner();
+		if (owner != null && TagData.getCombat((IEntityDataSaver) owner)){
 			((Entity) (Object) this).discard();
 			ci.cancel();
 		}
